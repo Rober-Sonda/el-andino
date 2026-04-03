c97e92b3fe95cef5d5d7f901c751d6b6694160a6
robersonda <rober.junin@gmail.com>
2026-04-03 20:08:29 -0300

    fix: Restore text and icon combo logo to navbar and increase height

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ const Navbar = () => {
   return (
     <nav className={`navbar ${scrolled ? 'scrolled' : ''}`}>
       <div className="logo" style={{ display: 'flex', alignItems: 'center' }}>
-        <img src="/favicon.png" alt="El Andino Logo" className="nav-logo-img" style={{ height: '40px', objectFit: 'contain' }} />
+        <img src="/logo_nav.png" alt="El Andino Logo" className="nav-logo-img" style={{ height: '50px', objectFit: 'contain' }} />
       </div>
       
       <div style={{display: 'flex', alignItems: 'center'}}>
